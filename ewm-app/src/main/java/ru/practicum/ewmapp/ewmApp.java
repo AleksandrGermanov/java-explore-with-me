c59e5babdf0ea7218e47a74d75db7a597272965c
Александр Германов <mailmate@yandex.ru>
2023-11-30 01:57:52 +0300

    feat: написать схему основного сервиса

---
+++ ewm-app/src/main/java/ru/practicum/ewmapp/ewmApp.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ewmApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ewmApp.class);
+    }
+}
---
+++ ewm-app/src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+--TODO удалить первую инструкцию при переносе в прод.
+DROP TABLE IF EXISTS users, categories, compilations, events, compilations_events, participation_requests;
+
+CREATE TABLE IF NOT EXISTS users(
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name VARCHAR(128) NOT NULL,
+email VARCHAR(128) NOT NULL UNIQUE,
+);
+
+CREATE TABLE IF NOT EXISTS categories(
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name VARCHAR(64) NOT NULL UNIQUE,
+);
+
+CREATE TABLE IF NOT EXISTS compilations(
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+title VARCHAR(64) NOT NULL UNIQUE,
+pinned BOOLEAN NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS events(
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+annotation VARCHAR(256) NOT NULL,
+category_id BIGINT NOT NULL UNIQUE REFERENCES categories(id) ON DELETE RESTRICT,
+confirmed_requests BIGINT,
+created_on TIMESTAMP NOT NULL,
+description VARCHAR(512),
+event_date TIMESTAMP NOT NULL,
+initiator_id BIGINT BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+location_lat FLOAT NOT NULL,
+location_lon FLOAT NOT NULL,
+paid BOOLEAN  NOT NULL,
+participant_limit INT,
+published_on TIMESTAMP,
+request_moderation BOOLEAN,
+state VARCHAR(16),
+title VARCHAR(64) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS compilations_events(
+compilation_id BIGINT NOT NULL REFERENCES compilations(id) ON DELETE CASCADE,
+event_id BIGINT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+CONSTRAINT pk PRIMARY KEY (compilation_id, event_id)
+);
+
+CREATE TABLE IF NOT EXISTS participation_requests(
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+created TIMESTAMP NOT NULL,
+event_id BIGINT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+requester_id BIGINT NOT REFERENCES users(id) ON DELETE CASCADE,
+status VARCHAR(16) NOT NULL
+);
