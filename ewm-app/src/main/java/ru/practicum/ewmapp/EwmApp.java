8adfb14e9fce29d8852c911774b74071721cfb96
Александр Германов <mailmate@yandex.ru>
2023-12-20 23:37:47 +0300

    delete: удалить @Config EndpointHitInjector.java. refactor: добавить аннотацию @C@ComponentScan("ru.practicum.endpointhitclient") для Ewm.App

--- ewm-app/src/main/java/ru/practicum/ewmapp/EwmApp.java
+++ ewm-app/src/main/java/ru/practicum/ewmapp/EwmApp.java
@@ -2,8 +2,11 @@ package ru.practicum.ewmapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("ru.practicum.endpointhitclient")
 public class EwmApp {
     public static void main(String[] args) {
         SpringApplication.run(EwmApp.class);
